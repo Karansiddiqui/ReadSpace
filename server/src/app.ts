26efa44e79e358fdfca1a4c3572e06d10288911c
Karansiddiqui <karansiddiq@example.com>
2024-10-04 14:47:59 +0530

    Done

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,8 @@ import { Request, Response, NextFunction } from "express";
 import { ApiError } from "./utils/ApiError.js";
 import userRoutes from "./routes/user.route.js";
 import userTransaction from "./routes/transaction.route.js";
+import path from "path";
+
 const app: Application = express();
 
 app.use(
@@ -14,6 +16,8 @@ app.use(
     credentials: true,
   })
 );
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
@@ -25,6 +29,12 @@ app.use(cookieParser());
 app.use("/api/books", bookRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/transaction", userTransaction);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((error: ApiError, req: Request, res: Response, next: NextFunction) => {
   const errorMessage = error.message || "Something went wrong";
